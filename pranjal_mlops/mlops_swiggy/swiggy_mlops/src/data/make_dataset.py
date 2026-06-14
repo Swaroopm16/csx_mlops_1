1b074dd1a1fdc12755b322d0eae51e7fb2b01e21
Swaroop <swaroop.m2005@gmail.com>
2026-06-14 12:06:14 +0530

    introducing the cookiecutter in project

---
+++ pranjal_mlops/mlops_swiggy/swiggy_mlops/src/data/make_dataset.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import click
+import logging
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+
+def read_csv(file_path);
+  return pd.read_csv(file_path)
+
